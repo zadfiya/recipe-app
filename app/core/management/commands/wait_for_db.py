e81d48aaeaa6f8a8b71e2b0eddcdb3af1863da5e
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-02-14 23:20:21 -0500

    Module not found error resolved

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -24,4 +24,5 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+        self.stdout.write('Database available')
 
