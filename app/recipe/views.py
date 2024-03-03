8d2759c70cbcdfd7e026f006a06d825528691fa3
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-02 23:56:45 -0500

    TagView rypo error

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
-class TagViewSetclass(mixins.DestroyModelMixin,
+class TagViewSet(mixins.DestroyModelMixin,
                  mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
