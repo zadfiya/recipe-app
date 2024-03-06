f8f00a52ea26a1b027934536d5b8324ab88bd168
Naren Zadafiya <narenzadfiya3579@gmail.com>
2024-03-06 18:17:51 -0500

    Implementation of Image Path generation fn

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,8 @@
 """
 Database models.
 """
+import uuid
+import os
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -8,6 +10,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image."""
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
